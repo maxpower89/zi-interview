6cf69b4b02dd20630582977e789e634b1c692419
Mark Johan Smit <mark.s@investing.com>
2022-02-04 15:30:29 +0200

    replaced to return rather than console log

--- be/hierarchy/hierarchy.controller.js
+++ be/hierarchy/hierarchy.controller.js
@@ -3,9 +3,10 @@ const service= require("./hierarchy.service")
 async function getHierarchyForId(id) {
    let parents=await service.getParentsForId(id);
    const result=parents.join(" -> ")
-   console.log( result );
+   return result;
 }
 
 
-const id = 9;
-getHierarchyForId(id);
+module.exports = {
+   getHierarchyForId
+};
--- be/hierarchy/hierarchy.routes.js
+++ be/hierarchy/hierarchy.routes.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const _ = require("lodash");
+const controller = require('./hierarchy.controller');
 const router = express.Router();
 
 
 
+router.get('/:id', getHierarchyForId);
+
+async function getHierarchyForId(req, res, next) {
+    try {
+        console.log(`[hierarchy] getHierarchyById, id: ${req.params.id}`);
+        const hierarchy = await controller.getHierarchyForId(req.params.id);
+        res.status(200).send(hierarchy);
+    }
+    catch (err) {
+        const errMessage = _.get(err, 'message', 'error occurred');
+        const errCode = _.get(err, 'status', 500);
+        res.status(errCode).json({message: 'error occurred', error: errMessage});
+    }
+}
+``
 
 module.exports = router;
